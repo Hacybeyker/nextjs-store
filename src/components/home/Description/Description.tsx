bdabce530e809ca110d8df387d74bdf4e29f94f0
Carlos Osorio <hacybeyker@gmail.com>
2025-04-26 18:01:01 -0500

    Actualiza el componente layout.tsx para utilizar la fuente Montserrat en lugar de Inter, mejorando la tipografía de la aplicación. Además, se agrega soporte para estilos responsivos en el componente Description al incluir la propiedad sizes en la imagen, optimizando su visualización en diferentes dispositivos.

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -12,6 +12,7 @@ export const Description = () => {
           src="/images/description.jpeg"
           alt="products marketplace"
           fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           priority={true}
           quality={100}
           placeholder="blur"

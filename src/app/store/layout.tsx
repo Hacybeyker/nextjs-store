2b707735b809801076bfd12a404090bc3185f774
Carlos Osorio <hacybeyker@gmail.com>
2025-04-28 11:20:10 -0500

    Agrega un archivo de ejemplo .env con variables de entorno para Shopify y actualiza .gitignore para incluir .env. Se añaden logs en los componentes layout.tsx y MainProducts.tsx para mostrar las variables de entorno correspondientes.

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,4 +1,5 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
+  console.log('variable de entorno: ', process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME);
   return (
     <main>
       <nav>Navigacion de las categorias</nav>

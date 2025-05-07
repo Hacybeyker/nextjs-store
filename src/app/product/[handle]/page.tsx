50e6b78df1d21ca1d9bd53a4b79f9dfd7b13eb9e
Carlos Osorio <hacybeyker@gmail.com>
2025-05-07 07:56:56 -0500

    Refactoriza la página del producto para mejorar la obtención de parámetros y actualiza la ruta de importación del componente ProductView. Además, se corrige la URL en el componente ProductCard para reflejar la estructura de rutas actualizada.

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,16 +1,21 @@
-import { ProductView } from '@/components/product/ProductView';
 import { getProducts } from '@/services/shopify/products';
+import { ProductView } from '@/components/product/ProductView/ProductView';
 
 interface ProductPageProps {
+  params: {
+    handle: string;
+  };
   searchParams: {
     id: string;
   };
 }
 
-export default async function ProductPage({ searchParams }: ProductPageProps) {
-  const id = searchParams.id;
+async function ProductPage(props: ProductPageProps) {
+  const { id } = await Promise.resolve(props.searchParams);
   const products = await getProducts(id);
   const product = products[0];
 
   return <ProductView product={product} />;
 }
+
+export default ProductPage;

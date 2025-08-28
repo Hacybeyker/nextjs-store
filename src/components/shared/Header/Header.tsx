ba24c0f68544b1add8e796ce337f906d93376de4
Carlos Osorio <hacybeyker@gmail.com>
2025-08-28 16:05:42 -0500

    feat: refactor Header to use ShoppingCartWrapper component for better SSR handling

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,9 +1,7 @@
 import Link from 'next/link';
 import { validateAccessToken } from '@/utils/auth/validateAccessToken';
+import { ShoppingCartWrapper } from './ShoppingCartWrapper';
 import styles from './Header.module.css';
-import dynamic from 'next/dynamic';
-
-const NoSSRShoppingCart = dynamic(() => import('../ShoppingCart'), { ssr: false });
 
 export const Header = async () => {
   const customer = await validateAccessToken();
@@ -31,7 +29,7 @@ export const Header = async () => {
       </nav>
       <div className={styles.Header__user}>
         {customer?.firstName ? <p>Hola! {customer.firstName}</p> : <Link href="/login">Login</Link>}
-        <NoSSRShoppingCart />
+        <ShoppingCartWrapper />
       </div>
     </header>
   );
---
+++ src/components/shared/Header/ShoppingCartWrapper.tsx
@@ -0,0 +1,8 @@
+'use client';
+import dynamic from 'next/dynamic';
+
+const NoSSRShoppingCart = dynamic(() => import('../ShoppingCart'), { ssr: false });
+
+export const ShoppingCartWrapper = () => {
+  return <NoSSRShoppingCart />;
+};

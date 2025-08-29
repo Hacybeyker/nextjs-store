93dae76cd4193afd33041c8dfc08eaf5fea365c7
Carlos Osorio <hacybeyker@gmail.com>
2025-08-29 00:41:31 -0500

    feat: add chat functionality with OpenAI integration and create Chat component

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,19 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
+  const { messages } = await req.json();
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const result = await streamText({
+    model: openai('gpt-4o-mini'),
+    messages,
+  });
+
+  // Respond with the stream
+  return result.toTextStreamResponse();
+}
---
+++ src/app/chat/page.tsx
@@ -0,0 +1,10 @@
+//import { Chat } from '@/components/chat/chat';
+//import { MainProducts } from '@/components/home/MainProducts';
+
+export default function ChatPage() {
+  return (
+    <>
+      <h1>Chatbot</h1>
+    </>
+  );
+}
